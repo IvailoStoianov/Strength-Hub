b6e0db0d29d331442de65b09bdd2206e27ea6426
StIvail0 <Ivailobg95@gmail.com>
2024-01-29 13:58:02 +0200

    First commit

---
+++ Js/ChangeCards.js
@@ -0,0 +1,21 @@
+function changeCard(event) {
+    let clickedCard = event.currentTarget; 
+    let currentCard = document.getElementById('crr-card');
+
+    if (clickedCard.id !== 'crr-card') {
+        
+        if (currentCard) {
+            currentCard.removeAttribute('id');
+            let currentCardName = currentCard.querySelector('.name');
+            if (currentCardName) {
+                currentCardName.classList.add('hidden');
+            }
+        }
+
+        clickedCard.id = 'crr-card';
+        let clickedCardName = clickedCard.querySelector('.name');
+        if (clickedCardName) {
+            clickedCardName.classList.remove('hidden');
+        }
+    }
+}
